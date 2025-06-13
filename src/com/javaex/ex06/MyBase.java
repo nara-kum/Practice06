d4a43347c60b18c3cf90602be2449d8fd93cff56
nara-kum <0603skfk@gmail.com>
2025-06-13 17:44:29 +0900

    다시연습해

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -3,5 +3,24 @@ package com.javaex.ex06;
 public class MyBase extends Base{
 
     //코드작성
+	//메소드일반
+	public void service(String state) {
+
+    if (state.equals("낮")) { //문자열의 같다(==) 는 equals()를 사용합니다.
+        day();
+    } else if(state.equals("오후")) {
+    	noon();
+    } else {
+        night();
+    }
+}
+	public void day() {
+        System.out.println("낮에는 열심히 수업듣자");
+    }
+
+    public void noon() {
+        System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+    }
+	
     
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -5,12 +5,26 @@ public class SmartPhone extends MusicPhone {
     public void execute(String str){
         
         //코드작성
-        
+        if("음악".equals(str)){
+            downloadMusic();
+        }else if("앱".equals(str)){
+        	app();
+        }else {
+        	super.execute(str);
+        }
+    	
     }
  
     //메소드작성
+    protected void downloadMusic(){
+        System.out.println("다운로드해서 음악재생");
+    }
     
     //메소드작성
+    
+    protected void app(){
+        System.out.println("앱실행");
+    }
     
     
     
